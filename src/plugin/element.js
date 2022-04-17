b0d5cfe701868b7ad49425ef1b3309e8896f77ac
zhouqichao <zhouqc@heywhale.com>
2022-04-18 00:17:44 +0800

    ✨ feat(all): 添加高德地图逆地址解析

--- src/plugin/element.js
+++ src/plugin/element.js
@@ -8,11 +8,13 @@ import {
   Input,
   Tooltip,
   Message,
+  Select,
+  Option,
 } from 'element-ui';
 import 'element-ui/lib/theme-chalk/index.css';
 
-[Icon, Form, FormItem, Link, Button, Input, Tooltip].forEach((comp) =>
-  Vue.use(comp)
+[Icon, Form, FormItem, Link, Button, Input, Tooltip, Select, Option].forEach(
+  (comp) => Vue.use(comp)
 );
 
 Vue.prototype.$message = Message;
